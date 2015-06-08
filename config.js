ba8ab8033a19d94d4f42585104b69c428cdd5df1
Damiano <dunkeltaenzer@googlemail.com>
2015-06-08 02:22:34 +0200

    background now blends better into layout, hope works on bigger monitors too... added config.js with R/O twitter key to allow team out-of-the-box use. remove after event.

---
+++ config.js
@@ -0,0 +1,4 @@
+	exports.consumer_key = 'z0bLZQf7TAAA9GPEeIUqIc0Sq';
+	exports.consumer_secret = 'EzdYKeOkr60A3aNUPoEOcNWbtwkY9AybBjZy9Q9ddRwUCFoXqu';
+	exports.access_token_key = '417490489-Ou1qbbb9w7Bq5RJCVrwiMq4xqSSvXs6mv3dIb0Ju';
+	exports.access_token_secret = '8E1EVBfZEePRRwTPfU1RkJHZa1QsO6UUB7NXHzhWJutcc';
